046a3ae3fbc450575443b00378c8d5de5a235281
philip-morlier <philipmorlier@gmail.com>
2021-09-09 12:30:27 -0700

    Modifications to hello/main.go, go.mod, and go.sum to adress dependency issues

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/openrelayxyz/plugeth-plugins
 
 go 1.16
 
-require github.com/openrelayxyz/plugeth-utils v0.0.4
+require (
+	github.com/openrelayxyz/plugeth-utils v0.0.4
+	gopkg.in/urfave/cli.v1 v1.20.0
+)
--- packages/hello/main.go
+++ packages/hello/main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-  "github.com/openrelayxyz/plugeth-utils/core"
+	"github.com/openrelayxyz/plugeth-utils/core"
+	"gopkg.in/urfave/cli.v1"
 )
 
 var (
-  logger core.Logger
+	log core.Logger
 )
 
 type myservice struct{}
 
 func (*myservice) Hello() string {
-  return "Hello world"
+	return "Hello world"
 }
 
-func InitializeNode(l core.Logger, node core.Node, backend core.Backend) {
-  logger = l
-  logger.Info("Initialized hello")
+func Initialize(ctx *cli.Context, loader core.PluginLoader, logger core.Logger) {
+	log = logger
+	log.Info("Initialized hello")
 }
 
 func GetAPIs(node core.Node, backend core.Backend) []core.API {
-  defer logger.Info("APIs Initialized")
-  return []core.API{
-    {
-      Namespace: "mynamespace",
-      Version: "1.0",
-      Service: &myservice{},
-      Public: true,
-    },
-  }
+	defer log.Info("APIs Initialized")
+	return []core.API{
+		{
+			Namespace: "mynamespace",
+			Version:   "1.0",
+			Service:   &myservice{},
+			Public:    true,
+		},
+	}
 }
