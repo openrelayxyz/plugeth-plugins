18f480c228d3f091382449c51d6db5d234908721
philip-morlier <philipmorlier@gmail.com>
2022-01-21 13:19:38 -0800

    Further updates to gethpartity function per findings of intergration testing.

--- packages/parity-trace-replaytransaction/main.go
+++ packages/parity-trace-replaytransaction/main.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/openrelayxyz/plugeth-utils/core"
-	"github.com/openrelayxyz/plugeth-utils/restricted/hexutil"
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -24,27 +23,17 @@ type InnerResult struct {
 }
 
 type Action struct {
-	CallType string         `json:"callType,omitempty"`
-	From     string         `json:"from,omitempty"`
-	Gas      string         `json:"gas,omitempty"`
-	Init     string         `json:"init,omitempty"`
-	Input    string         `json:"input,omitempty"`
-	To       string         `json:"to,omitempty"`
-	Value    hexutil.Uint64 `json:"value"`
+	CallType      string `json:"callType,omitempty"`
+	From          string `json:"from,omitempty"`
+	Address       string `json:"address,omitempty"`
+	Balance       string `json:"balance,omitempty"`
+	Gas           string `json:"gas,omitempty"`
+	Init          string `json:"init,omitempty"`
+	Input         string `json:"input,omitempty"`
+	To            string `json:"to,omitempty"`
+	RefundAddress string `json:"refundAddress,omitempty"`
+	Value         string `json:"value,omitempty"`
 }
-
-// type CreateAction struct {
-// 	From  string         `json:"from"`
-// 	Gas   string         `json:"gas"`
-// 	Init  string         `json:"init"`
-// 	Value hexutil.Uint64 `json:"value"`
-// }
-//
-// type CreateInnerResult struct {
-// 	Address string `json:"address"`
-// 	Code    string `json:"code"`
-// 	GasUsed string `json:"gasUsed"`
-// }
 
 type ParityResult struct {
 	Action        *Action      `json:"action"`
@@ -92,7 +81,7 @@ type GethResponse struct {
 	Type    string         `json:"type,omitempty"`
 	From    string         `json:"from,omitempty"`
 	To      string         `json:"to,omitempty"`
-	Value   hexutil.Uint64 `json:"value,omitempty"`
+	Value   string         `json:"value,omitempty"`
 	Gas     string         `json:"gas,omitempty"`
 	GasUsed string         `json:"gasUsed,omitempty"`
 	Input   string         `json:"input,omitempty"`
@@ -105,7 +94,7 @@ func FilterPrecompileCalls(calls []GethResponse) []GethResponse {
 	result := []GethResponse{}
 	for _, call := range calls {
 		//develop test case to see what parity does if is legit call to procompiled contract
-		if !strings.HasPrefix(call.To, "0x000000000000000000000000000000000000") || call.Value != 0 {
+		if !strings.HasPrefix(call.To, "0x000000000000000000000000000000000000") || call.Value != "" {
 			result = append(result, call)
 		}
 	}
@@ -117,7 +106,45 @@ func GethParity(gr GethResponse, address []int, t string) []*ParityResult {
 	calls := FilterPrecompileCalls(gr.Calls)
 	addr := make([]int, len(address))
 	copy(addr[:], address)
+	if string(gr.GasUsed) == "" {
+		gr.GasUsed = "0x0"
+	}
+	if gr.Output == "" {
+		gr.Output = "0x"
+	}
+	if gr.Value == "" {
+		gr.Value = "0x0"
+	}
+	unique := 0
 	if gr.Error == "execution reverted" {
+		unique = 1
+	}
+	if gr.Type == "CREATE" || gr.Type == "CREATE2" {
+		unique = 2
+	}
+	// if gr.Gas <= gr.GasUsed
+	if gr.Error == "max code size exceeded" {
+		unique = 3
+	}
+	if gr.Type == "SELFDESTRUCT" {
+		unique = 4
+	}
+	switch unique {
+	case 0:
+		result = append(result, &ParityResult{
+			Action: &Action{CallType: strings.ToLower(gr.Type),
+				From:  gr.From,
+				Gas:   gr.Gas,
+				Input: gr.Input,
+				To:    gr.To,
+				Value: gr.Value},
+			Result: &InnerResult{GasUsed: gr.GasUsed,
+				Output: gr.Output},
+			SubTraces:     len(calls),
+			TracerAddress: addr,
+			Type:          t})
+
+	case 1:
 		result = append(result, &ParityResult{
 			Action: &Action{CallType: strings.ToLower(gr.Type),
 				From:  gr.From,
@@ -129,8 +156,8 @@ func GethParity(gr GethResponse, address []int, t string) []*ParityResult {
 			SubTraces:     len(calls),
 			TracerAddress: addr,
 			Type:          t})
-	}
-	if gr.Type == "CREATE" {
+
+	case 2:
 		result = append(result, &ParityResult{
 			Action: &Action{
 				From:  gr.From,
@@ -144,21 +171,33 @@ func GethParity(gr GethResponse, address []int, t string) []*ParityResult {
 			},
 			SubTraces:     len(calls),
 			TracerAddress: addr,
-			Type:          t})
-	} else {
+			Type:          "create"})
+
+	case 3:
 		result = append(result, &ParityResult{
-			Action: &Action{CallType: strings.ToLower(gr.Type),
+			Action: &Action{
 				From:  gr.From,
 				Gas:   gr.Gas,
-				Input: gr.Input,
-				To:    gr.To,
+				Init:  gr.Input,
 				Value: gr.Value},
-			Result: &InnerResult{GasUsed: gr.GasUsed,
-				Output: gr.Output},
+			Error:         "Out of gas",
 			SubTraces:     len(calls),
 			TracerAddress: addr,
 			Type:          t})
+
+	case 4:
+		balance := gr.Value
+		result = append(result, &ParityResult{
+			Action: &Action{
+				Address:       gr.From,
+				Balance:       balance,
+				RefundAddress: gr.To},
+			Result:        &InnerResult{},
+			SubTraces:     len(calls),
+			TracerAddress: addr,
+			Type:          "suicide"})
 	}
+
 	for i, call := range calls {
 		if call.Type == "DELEGATECALL" {
 			call.Value = gr.Value
@@ -177,6 +216,9 @@ func (ap *APIs) ReplayTransaction(ctx context.Context, txHash core.Hash, types [
 	client.Call(&gr, "debug_traceTransaction", txHash, map[string]string{"tracer": "callTracer"})
 	tAddress := make([]int, 0)
 	gp := GethParity(gr, tAddress, strings.ToLower(gr.Type))
+	if gr.Output == "" {
+		gr.Output = "0x"
+	}
 	result := &OuterResult{
 		Output:    gr.Output,
 		StateDiff: nil,
