2cf256f1ab285e768a8597e4c4fb73504a8c2f81
philip-morlier <philipmorlier@gmail.com>
2023-02-17 14:23:58 -0800

    draft of flushInterval rpc clone

--- packages/forceAllowCacheFlush/main.go
+++ packages/forceAllowCacheFlush/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"context"
 	"github.com/openrelayxyz/plugeth-utils/core"
 )
@@ -12,7 +13,7 @@ type FlushCacheService struct {
 
 func Initialize(ctx core.Context, loader core.PluginLoader, logger core.Logger) {
 	log = logger
-	log.Info("loaded Flush-Dirty-Cache plugin")
+	log.Info("loaded Allow Force trei flush plugin")
 }
 
 func GetAPIs(stack core.Node, backend core.Backend) []core.API {
@@ -26,14 +27,66 @@ func GetAPIs(stack core.Node, backend core.Backend) []core.API {
 	}
 }
 
+var nodeInterval time.Duration 
+
+func PauseTreiCommit(gcproc, duration time.Duration) time.Duration {
+	log.Error("these are the values", "gcproc", gcproc, "duration", duration)
+	if nodeInterval.Minutes() > 0 {
+		duration = nodeInterval
+	}
+	return duration
+}
+
+// or something like where we inspect the gcproc and if we are in puase mode we repeatedly call the function and incredment by some t
+// the tickher inspector and incrementor would need to be started and stopped via rpc. Or have a switch that is always false until it is true. 
+
+func (service *FlushCacheService) InspectTrieInterval(ctx context.Context) string {
+	return nodeInterval.String()
+}
+
+func (service *FlushCacheService) ModifyTrieInterval(ctx context.Context, arg string) (string, error) {
+	newInterval, err := time.ParseDuration(arg)
+	if err != nil {
+		return "", err
+	}
+	nodeInterval = newInterval
+
+	return "flushInterval has been modified", nil
+}
+
 var val bool 
+
+var allowVal bool = true
+
+func AllowTreiCommit() bool {
+	defer func() {
+		allowVal = true
+	}()
+	return allowVal
+}
+
+func ForceTreiCommit() bool {
+	defer func() {
+		val = false
+	}()
+	return val
+}
 
 func FlushCache() bool {
 	defer func() {
 		val = false
 	}()
-
 	return val
+}
+
+func (service *FlushCacheService) CallAllowTreiCommit(ctx context.Context) string {
+	AllowTreiCommit()
+	return "Trie is allowed to flush"
+}
+
+func (service *FlushCacheService) CallForceTreiCommit(ctx context.Context) string {
+	val = true
+	return "Trie has been forced to flush"
 }
 
 func (service *FlushCacheService) CallFlushCache(ctx context.Context) string {
