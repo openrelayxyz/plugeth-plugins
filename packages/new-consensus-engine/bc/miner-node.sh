2cda5500e0920f9d6ba356471787f8bbf3a573b2
philip-morlier <philipmorlier@gmail.com>
2023-04-04 21:29:04 -0700

    renamed node/ to bc/ added passive and miner bash

---
+++ packages/new-consensus-engine/bc/miner-node.sh
@@ -0,0 +1,17 @@
+rm -r 00/ test00/  
+rm passwordfile
+mkdir -p test00/ 00/keystore  00/geth  00/plugins 
+cp ../engine.go test00/ 
+cd test00/ 
+go build -buildmode=plugin -o ../00/plugins
+cd ../
+cp UTC--2021-03-02T16-47-49.510918858Z--f2c207111cb6ef761e439e56b25c7c99ac026a01 00/keystore
+cp nodekey00 00/geth/nodekey
+
+echo -n "supersecretpassword" > passwordfile
+
+$GETH init --datadir=./00 genesis.json
+
+$GETH --config config00.toml --authrpc.port 8552 --port 64480 --verbosity 5 --nodiscover --networkid=6448 --datadir=./00/ --mine --miner.etherbase f2c207111cb6ef761e439e56b25c7c99ac026a01 --unlock f2c207111cb6ef761e439e56b25c7c99ac026a01 --http --http.api eth,debug,net --http.port 9545 --password passwordfile --allow-insecure-unlock & 
+
+wait
---
+++ packages/new-consensus-engine/bc/passive-node.sh
@@ -0,0 +1,17 @@
+rm -r 01/ test01/ 
+rm passwordfile
+mkdir -p test01/ 01/keystore 01/geth 01/plugins
+cp ../engine.go ../main.go ../plugins.go  test01/
+cd test01/ 
+go build -buildmode=plugin -o ../01/plugins
+cd ../
+cp UTC--2021-03-02T16-47-39.492920333Z--4204477bf7fce868e761caaba991ffc607717dbf 01/keystore
+cp nodekey01 01/geth/nodekey
+
+echo -n "supersecretpassword" > passwordfile
+
+$GETH init --datadir=./01 genesis.json
+
+$GETH --config config01.toml --authrpc.port 8553 --port 64481 --nodiscover --networkid=6448 --datadir=./01/ --unlock 4204477bf7fce868e761caaba991ffc607717dbf --miner.etherbase 4204477bf7fce868e761caaba991ffc607717dbf --password passwordfile --ws --ws.port 8546 --ws.api eth,admin --http --http.api eth,debug,net --allow-insecure-unlock &
+
+wait
