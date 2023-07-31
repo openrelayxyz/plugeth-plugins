5884988ead1b746ea933b4181abb5a3157d438ee
philip-morlier <philipmorlier@gmail.com>
2023-07-31 13:28:41 -0700

    Changes made to consensus engine to support bor and etc.

--- packages/consensus-engine/chain/bor-geth.sh
+++ packages/consensus-engine/chain/bor-geth.sh
@@ -23,15 +23,13 @@ echo -n "supersecretpassword" > passwordfile
 $GETH init --datadir=./00 bor-genesis.json
 $GETH init --datadir=./01 bor-genesis.json
 
-$GETH --cache.preimages --config ./config00.toml --authrpc.port 8552 --port 64480 --verbosity=0 --nodiscover --networkid=6448 --datadir=./00/ --mine --miner.noverify --miner.etherbase f2c207111cb6ef761e439e56b25c7c99ac026a01 --unlock f2c207111cb6ef761e439e56b25c7c99ac026a01 --http --http.api eth,debug,net,admin --http.port 9545 --password passwordfile --allow-insecure-unlock &
+$GETH --cache.preimages --config=./config00.toml --authrpc.port 8552 --port 64480 --verbosity=0  --nodiscover --networkid=6448 --datadir=./00/ --mine --miner.noverify --miner.etherbase f2c207111cb6ef761e439e56b25c7c99ac026a01 --unlock f2c207111cb6ef761e439e56b25c7c99ac026a01 --http --http.api eth,debug,net,admin --http.port 9545 --password passwordfile --allow-insecure-unlock --rpc.allow-unprotected-txs &
 pid0=$!
-$GETH --cache.preimages --config ./config01.toml --authrpc.port 8553 --port 64481 --verbosity=3 --syncmode=full --nodiscover --networkid=6448 --datadir=./01/ --unlock 4204477bf7fce868e761caaba991ffc607717dbf --miner.etherbase 4204477bf7fce868e761caaba991ffc607717dbf --password passwordfile --ws --ws.port 8546 --ws.api eth,admin --http --http.api eth,debug,net,admin --http.port 9547 --allow-insecure-unlock &
-# pid1=$!
+$GETH --cache.preimages --config=./config01.toml --authrpc.port 8553 --port 64481 --verbosity=3 --syncmode=full --nodiscover --networkid=6448 --datadir=./01/ --unlock 4204477bf7fce868e761caaba991ffc607717dbf --miner.etherbase 4204477bf7fce868e761caaba991ffc607717dbf --password passwordfile --ws --ws.port 8546 --ws.api eth,admin --http --http.api eth,debug,net,admin --http.port 9547 --allow-insecure-unlock --rpc.allow-unprotected-txs &
 
 sleep 2
 
-python3 poly_curl.py
-
+python3 connect_nodes.py # This will need to be revisited in the future. The node keys from the config.toml *should be configuarable to make this unecessary. 
 
 sleep 5
 
