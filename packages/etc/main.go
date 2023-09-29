553bffd2da0b4b26bd6476f8f265268ba78715be
philip-morlier <philipmorlier@gmail.com>
2023-09-28 21:21:28 -0700

    Config plugins

--- packages/etc/main.go
+++ packages/etc/main.go
@@ -3,8 +3,25 @@ package main
 import (
 	"context"
 	"path/filepath"
+	"strings"
 
 	"github.com/openrelayxyz/plugeth-utils/core"
+)
+
+var (
+	ClassicBootnodes = []string{
+
+		"enode://942bf2f0754972391467765be1d98206926fc8ad0be8a49cd65e1730420c37fa63355bddb0ae5faa1d3505a2edcf8fad1cf00f3c179e244f047ec3a3ba5dacd7@176.9.51.216:30355", // @q9f ceibo
+		"enode://0b0e09d6756b672ac6a8b70895da4fb25090b939578935d4a897497ffaa205e019e068e1ae24ac10d52fa9b8ddb82840d5d990534201a4ad859ee12cb5c91e82@176.9.51.216:30365", // @q9f ceibo
+
+		"enode://b9e893ea9cb4537f4fed154233005ae61b441cd0ecd980136138c304fefac194c25a16b73dac05fc66a4198d0c15dd0f33af99b411882c68a019dfa6bb703b9d@18.130.93.66:30303",
+	}
+
+	dnsPrefixETC string = "enrtree://AJE62Q4DUX4QMMXEHCSSCSC65TDHZYSMONSD64P3WULVLSF6MRQ3K@"
+
+	ClassicDNSNetwork1 string = dnsPrefixETC + "all.classic.blockd.info"
+
+	snapDiscoveryURLs []string
 )
 
 type ClassicService struct {
@@ -42,11 +59,6 @@ func DefaultDataDir(path string) string {
 	return filepath.Join(path, "classic")
 }
 
-func SetBootstrapNodes() []string {
-	var classicBootnodes = []string{} // this will need to be modified?
-	return classicBootnodes
-}
-
 func SetNetworkId() *uint64 {
 	var networkId *uint64
 	classicNetworkId := uint64(1)
@@ -54,14 +66,25 @@ func SetNetworkId() *uint64 {
 	return networkId 
 }
 
-func SetETHDiscoveryURLs() []string {
-	var result []string
+func SetBootstrapNodes() []string {
+	result := ClassicBootnodes
+	return result
+}
+
+func SetETHDiscoveryURLs(lightSync bool) []string {
+
+	url := ClassicDNSNetwork1
+	if lightSync == true {
+		url = strings.ReplaceAll(url, "all", "les")
+	}
+	result := []string{url}
+	snapDiscoveryURLs = result
+
 	return result
 }
 
 func SetSnapDiscoveryURLs() []string {
-	var result []string
-	return result
+	return snapDiscoveryURLs
 }
 
 
