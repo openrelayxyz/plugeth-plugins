48f721b31f53c3b1e4536d07b8f9039a03614d90
Austin Roberts <austin.roberts@rivet.cloud>
2024-02-12 15:58:06 -0600

    Add blocking profiling

--- packages/blockpprof/main.go
+++ packages/blockpprof/main.go
@@ -7,9 +7,11 @@ import (
 	"os"
 	"time"
 	"github.com/openrelayxyz/plugeth-utils/core"
+	"runtime"
 	"runtime/pprof"
 	"path/filepath"
 	"flag"
+	"fmt"
 )
 
 var (
@@ -19,6 +21,7 @@ var (
 	lastNewHead time.Time
 	Flags = *flag.NewFlagSet("blockpprof", flag.ContinueOnError)
 	blockGapFlag = Flags.Int("pprof.block.gap", 6, "The number of seconds between blocks that should trigger a pprof flush")
+	blockProfile = Flags.Bool("pprof.block.profile", false, "Enable blocking profiling")
 	blockGapFolder = Flags.String("pprof.folder", "/var/lib/ethereum/pprofs", "Directory where pprof flushes should be written")
 	blockGapDuration = 12 * time.Second
 
@@ -29,6 +32,12 @@ func Initialize(ctx core.Context, loader core.PluginLoader, logger core.Logger) 
 	blockGapDuration = time.Duration(*blockGapFlag) * time.Second
 	buff = new(bytes.Buffer)
 	writer = bufio.NewWriter(buff)
+}
+
+func PreProcessBlock(hash core.Hash, num uint64, blockrlp []byte) {
+	if *blockProfile {
+		runtime.SetBlockProfileRate(1)
+	}
 }
 
 func NewHead(blockBytes []byte, hash core.Hash, logsBytes [][]byte, td *big.Int) {
@@ -42,11 +51,24 @@ func NewHead(blockBytes []byte, hash core.Hash, logsBytes [][]byte, td *big.Int)
 		} else {
 			log.Warn("Could not create file to flush pprof", "err", err, "path", pprofFilePath)
 		}
+		if *blockProfile {
+			blockProfileFilePath := filepath.Join(*blockGapFolder, fmt.Sprintf("%v.blockprof", hash.String()))
+			blockProfile, err := os.Create(blockProfileFilePath)
+			if err != nil {
+				log.Warn("could not create block profile", "err", err, "path", blockProfileFilePath)
+			}
+			defer blockProfile.Close()
+			if err := pprof.Lookup("block").WriteTo(blockProfile, 0); err != nil {
+				log.Warn("could not write block profile", "err", err, "block", hash)
+			}
+		}
+			
 	}
 	buff.Truncate(0)
 	writer.Reset(buff)
 	if err := pprof.StartCPUProfile(writer); err != nil {
 		log.Warn("Error starting cpu profile", "err", err)
 	}
+	runtime.SetBlockProfileRate(0) // Reset block profile rate after the function returns.
 	lastNewHead = time.Now()
 }
