5ffdd8690ae332f2e83df52d452834d4cbe8add0
Philip Morlier <philipmorlier@gmail.com>
2024-08-12 15:46:20 -0700

    Modified broker creation

--- packages/peerManager/kafka.go
+++ packages/peerManager/kafka.go
@@ -18,7 +18,8 @@ func createProducer(broker, topic string) (sarama.AsyncProducer, error) {
     brokers, config := transports.ParseKafkaURL(strings.TrimPrefix(broker, "kafka://"))
     configEntries := make(map[string]*string)
     configEntries["retention.ms"] = strPtr("3600000")
-    if err := transports.CreateTopicIfDoesNotExist(strings.TrimPrefix(broker, "kafka://"), topic, 0, configEntries); err != nil {
+
+    if err := transports.CreateTopicIfDoesNotExist(strings.TrimPrefix(broker, "kafka://"), topic, 1, configEntries); err != nil {
         panic(fmt.Sprintf("Could not create topic %v on broker %v: %v", topic, broker, err.Error()))
     }
     
